65660101090c6a4d2a297a6f0f46b46ade72826f
AemPi <markus.proepper@t-online.de>
2023-02-21 07:26:39 +0100

    Added AES Credential creation function

--- .config/default_module.psm1
+++ .config/default_module.psm1
@@ -347,8 +347,9 @@ function Send-Telegram()
       }
 
       $MessageBody = @"
+$($Hostname) $($Status_Icon)
 -----------------------------
---- $($Hostname) $($Status_Icon) ---
+--- $(Get-Date -Format "yyyy-MM-dd HH:mm:ss") ---
 -----------------------------
 $($T_Message)
 "@
@@ -371,6 +372,68 @@ $($T_Message)
     }
 }
 
+function New-AEScredentioals()
+{
+
+    Param(
+        [Parameter(Mandatory=$true,Position=1)][string]$CredentialName,
+        [Parameter(Mandatory=$false,Position=2)][string]$OutputPath
+    )
+
+    # Enter Key Name
+    $CredNames = $CredentialName
+
+    # Check if Folder Exists
+    if([system.string]::IsNullOrEmpty($OutputPath))
+    {
+        $KeyPath = "$env:USERPROFILE\Desktop\$($CredNames)_Creds"
+    }
+    else
+    {
+        $KeyPath = "$OutputPath\$($CredNames)_Creds"
+    }
+
+    if(-not (Test-Path $KeyPath))
+    {
+        mkdir $KeyPath | Out-Null
+    }
+
+    try
+    {
+        # Prompt you to enter the username and password
+        $credObject = Get-Credential
+        # The credObject now holds the password in a ‘securestring’ format
+        $passwordSecureString = $credObject.password
+
+        # Define a location to store the AESKey
+        $AESKeyFilePath = “$KeyPath\$CredNames" + "_aes.key”
+        # Define a location to store the file that hosts the encrypted password
+        $credentialFilePath = “$KeyPath\$CredNames" + "_Password.txt”
+
+        # Generate a random AES Encryption Key.
+        $AESKey = New-Object Byte[] 32
+        [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESKey)
+
+        # Store the AESKey into a file. This file should be protected! (e.g. ACL on the file to allow only select people to read)
+        Set-Content $AESKeyFilePath $AESKey # Any existing AES Key file will be overwritten
+        $password = $passwordSecureString | ConvertFrom-SecureString -Key $AESKey
+        Add-Content $credentialFilePath $password
+    }
+    catch
+    {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+        write-host $ErrorMessage
+    }
+
+    if((Test-Path $KeyPath))
+    {
+        Write-Host "Die Dateien liegen unter: "$KeyPath -BackgroundColor Green -ForegroundColor White
+        Start-Process $KeyPath
+    }
+
+}
+
 #############################################################
 # Definition der zu exportierenden Funktionen
 #############################################################
@@ -383,4 +446,5 @@ $FunctionsToExport = "Get-Uptime", `
                      "Invoke-MySQLQuery", `
                      "Disconnect-MySQL", `
                      "Test-RunAsAdmin", `
-                     "Send-Telegram"
+                     "Send-Telegram", `
+                     "New-AEScredentioals"
